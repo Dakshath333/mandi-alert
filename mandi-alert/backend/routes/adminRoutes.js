4d35cef476ee1acd8f2b9ffa8f3c49b9292177fb
Dakshath BS <dakshathbs3139@gmail.com>
2026-04-26 08:12:22 +0530

    Delete mandi-alert directory

--- mandi-alert/backend/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getStats, seedSampleData } = require('../controllers/adminController');
-
-router.get('/stats', getStats);
-router.post('/seed', seedSampleData);
-
-module.exports = router;
